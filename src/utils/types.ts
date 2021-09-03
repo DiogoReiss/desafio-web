8c530e908608f5b365ba029f16752f68a0e178e3
DiogoReiss <diogommreis1@gmail.com>
2021-09-03 02:16:12 +0100

    add: adding some types

---
+++ src/utils/types.ts
@@ -0,0 +1,13 @@
+export interface ITableCard {
+  title: string;
+  imgSRC: string;
+}
+
+export interface IButton {
+  content: string;
+  onclick: Function;
+}
+
+export interface IRouteParams {
+  tableTitle: string;
+}
